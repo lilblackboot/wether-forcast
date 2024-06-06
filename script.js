c11fd7c6182b1e407fdb431ddaff2630cb701cb0
lilblackboot <131806378+lilblackboot@users.noreply.github.com>
2024-06-07 00:42:52 +0530

    wireframe is done

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '3be9038ff1mshfe6b4e63e299a25p157364jsna90515af9724',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
